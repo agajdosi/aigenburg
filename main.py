50ffc384ebe9195c19ca25db7f909abe8a3e578b
Andreas Gajdosik <andreas@gajdosik.org>
2025-04-12 21:02:20 +0200

    Add some documentation

--- main.py
+++ main.py
@@ -4,13 +4,29 @@ from typing import Optional, Dict, Any
 
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 from openai import OpenAI
 from phoenix.client import Client
 from phoenix.client.utils import template_formatters
 from httpx import HTTPStatusError
 from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(
+    title="aigenburg API",
+    description=
+        "aigenburg is a dead simple API for unauthenticated requests to OpenAI or any other OpenAI compatible API provider (e.g. LiteLLM proxy). "
+        "Prompts are defined in Arize Phoenix server. Undefined prompts are not allowed. "
+        "This brings basic security as attackers can't call the API with arbitrary prompts.",
+    version="0.1.0",
+    contact={
+        "name": "aigenburg",
+        "url": "https://github.com/agajdosi/aigenburg",
+    },
+    openapi_url="/openapi.json",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    swagger_ui_parameters={"defaultModelsExpandDepth": -1},
+)
 
 # Configure CORS
 app.add_middleware(
@@ -20,8 +36,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
 
 # Dependency for OpenAI client
 def get_openai_client():
@@ -40,7 +54,6 @@ def get_phoenix_client():
 @app.get("/health")
 async def index():
     return "OK"
-
 
 class GenerateRequest(BaseModel):
     prompt_identifier: str
