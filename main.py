2b154c0df3815f492a604d10d27edfbaaf96fae4
Andreas Gajdosik <andreas@gajdosik.org>
2025-05-06 10:17:47 +0200

    Add crazy good Phoenix tracing!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from typing import Optional, Dict
+import json
 
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
@@ -9,6 +10,9 @@ from phoenix.client.utils import template_formatters
 from phoenix.otel import register
 from httpx import HTTPStatusError
 from pydantic import BaseModel
+from opentelemetry import trace
+from opentelemetry.trace import Status, StatusCode
+from openinference.semconv.trace import SpanAttributes, OpenInferenceSpanKindValues
 
 app = FastAPI(
     title="aigenburg API",
@@ -64,29 +68,40 @@ async def generate(
     openai_client: OpenAI = Depends(get_openai_client),
     phoenix_client: Client = Depends(get_phoenix_client)
 ):
-    """
-    Generate a response for a latest prompt version, filled with the provided variables.
-    """
-    try:
-        prompt_version = phoenix_client.prompts.get(prompt_identifier=request.prompt_identifier)
-    except HTTPStatusError as e:
-        raise HTTPException(
-            status_code=e.response.status_code,
-            detail=f"prompt_identifier not found. {str(e)}"
-        )
-        
-    # Format the prompt with the variables and request the LLM
-    try:
-        formatted_prompt = prompt_version.format(variables=request.prompt_variables or {})
-    except template_formatters.TemplateFormatterError as e:
-        raise HTTPException(
-            status_code=400,
-            detail=f"Error formatting prompt: {str(e)}"
-        )
+    """Generate a response for a latest prompt version, filled with the provided variables."""
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span(request.prompt_identifier) as span:
+        span.set_attribute(SpanAttributes.OPENINFERENCE_SPAN_KIND, OpenInferenceSpanKindValues.CHAIN.value)
+        span.set_attribute(SpanAttributes.INPUT_VALUE, json.dumps(request.prompt_variables))
+        try:
+            prompt_version = phoenix_client.prompts.get(prompt_identifier=request.prompt_identifier)
+            span.set_attribute("prompt.version_id", str(prompt_version.id))
+                
+        except HTTPStatusError as e:
+            detail = f"prompt_identifier not found. {str(e)}"
+            span.set_status(Status(StatusCode.ERROR), f"{detail} ({e.response.status_code})")
+            raise HTTPException(
+                status_code=e.response.status_code,
+                detail=detail
+            )
+            
+        # Format the prompt with the variables and request the LLM
+        try:
+            formatted_prompt = prompt_version.format(variables=request.prompt_variables or {})
+        except template_formatters.TemplateFormatterError as e:
+            detail = f"Error formatting prompt: {str(e)}"
+            span.set_status(Status(StatusCode.ERROR), detail)
+            raise HTTPException(
+                status_code=400,
+                detail=detail
+            )
 
-    resp = openai_client.chat.completions.create(**formatted_prompt)
-    resp_json = resp.model_dump_json()
-    return resp_json
+        resp = openai_client.chat.completions.create(**formatted_prompt)
+        resp_text = resp.choices[0].message.content
+        span.set_attribute(SpanAttributes.OUTPUT_VALUE, str(resp_text))
+        resp_json = resp.model_dump_json()
+        span.set_status(Status(StatusCode.OK))
+        return resp_json
 
 if __name__ == "__main__":
     collector_endpoint = os.environ.get("PHOENIX_COLLECTOR_ENDPOINT")
@@ -96,6 +111,7 @@ if __name__ == "__main__":
         project = os.environ.get("PHOENIX_OTEL_PROJECT", "aigenburg")
         print(f"Configuring Phoenix tracer for {project} to endpoint {collector_endpoint}")
         tracer_provider = register(project_name=project, auto_instrument=True)
+        #tracer = tracer_provider.get_tracer(__name__)
 
     import uvicorn
     uvicorn.run(
